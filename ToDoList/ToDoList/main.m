ee6a8735a7c16cb0189759a33031af4075ece8c3
Tommy Martin <gravity226@gmail.com>
2014-04-02 07:18:24 -0400

    To Do List; Empty App with Storyboard Added

---
+++ ToDoList/ToDoList/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Tommy Martin on 4/2/14.
+//  Copyright (c) 2014 Apple Developer Tutorial. All rights reserved.
+//
+// Tutorial provided by:
+// https://developer.apple.com/library/ios/referencelibrary/GettingStarted/RoadMapiOS/FirstTutorial.html#//apple_ref/doc/uid/TP40011343-CH3-SW1
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
