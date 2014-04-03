a7a2fc5c7a5b2f70d032110f02158867cbb77556
Tommy Martin <gravity226@gmail.com>
2014-04-03 07:36:48 -0400

    Insert Ninja

--- DragDrop/DragDrop/SpriteViewController.m
+++ DragDrop/DragDrop/SpriteViewController.m
@@ -1,6 +1,6 @@
 //
 //  SpriteViewController.m
-//  SpriteKitSimpleGame
+//  DragDrop
 //
 //  Created by Tommy Martin on 4/2/14.
 //  Copyright (c) 2014 Apple Developer Tutorial. All rights reserved.
@@ -11,23 +11,21 @@
 
 @implementation SpriteViewController
 
-- (void)viewWillLayoutSubviews
+- (void)viewDidLoad
 {
-    [super viewWillLayoutSubviews];
-    
+    [super viewDidLoad];
+
     // Configure the view.
     SKView * skView = (SKView *)self.view;
-    if (!skView.scene) {
-        skView.showsFPS = YES;
-        skView.showsNodeCount = YES;
-        
-        // Create and configure the scene.
-        SKScene * scene = [SpriteMyScene sceneWithSize:skView.bounds.size];
-        scene.scaleMode = SKSceneScaleModeAspectFill;
-        
-        // Present the scene.
-        [skView presentScene:scene];
-    }
+    skView.showsFPS = YES;
+    skView.showsNodeCount = YES;
+    
+    // Create and configure the scene.
+    SKScene * scene = [SpriteMyScene sceneWithSize:skView.bounds.size];
+    scene.scaleMode = SKSceneScaleModeAspectFill;
+    
+    // Present the scene.
+    [skView presentScene:scene];
 }
 
 - (BOOL)shouldAutorotate
